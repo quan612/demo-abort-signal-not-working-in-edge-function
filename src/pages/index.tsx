23c3ac0a874c95e21d69dcd2b91d9d2ee74a03cf
quan <quan612@yahoo.com>
2023-12-20 18:16:23 -0500

    test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -47,9 +47,17 @@ export default function StreamViewer() {
 
     return (
         <div style={{ margin: "4rem" }}>
-            {/* <a href="/" style={{ fontSize: "16px", marginBottom: "20px", color:"lightblue", textDecoration:"underline" }}>
-        To EventSource Demo 👉
-      </a> */}
+            <Link
+                href="/"
+                style={{
+                    fontSize: "16px",
+                    marginBottom: "20px",
+                    color: "lightblue",
+                    textDecoration: "underline",
+                }}
+            >
+                To EventSource Demo 👉
+            </Link>
             <h1 style={{ fontSize: "24px", fontWeight: "bold", marginBottom: "16px" }}>
                 Demo of Abort Signal not working with Edge (with fetch)
             </h1>
